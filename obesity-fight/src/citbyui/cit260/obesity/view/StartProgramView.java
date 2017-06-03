cc72c726d1f0f8dca93d312eae31baf99dc06832
die-zarry <zarrydie03@gmail.com>
2017-06-03 07:03:45 -0500

    our second commit second commit and first class done with the class layer model done with the different test cases as to the control layer class done with unit test cases for the loss weight percentage correct some minor bugs

---
+++ obesity-fight/src/citbyui/cit260/obesity/view/StartProgramView.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package citbyui.cit260.obesity.view;
+
+/**
+ *
+ * @author wdc
+ */
+public class StartProgramView {
+    private String promptMessage;
+    public StartProgramView(){
+    this.promptMessage = "\nPlease enter your name: ";
+    // display the banner when view is created
+    this.displayBanner();
+    
+  }
+
+    private void displayBanner() {
+       
+        System.out.println(
+        "\n********************"
+   + "\n*                    *"
+   + "\n* This is the game to fight Obesity*"
+   + "\n* In this game you will fight against obesity by contolling your diet *"
+   +"\n* and by selecting the appropriate exercice to do. *"
+   + "\n* Your goal is to fight aganst obesity*"
+   + "\n* your action will be the food you eat and the appropriate selected excercice*"
+   + "\n* you will measure your performance( winning or lossing)either according to your weight"
+   + "\n *loss or gain durring a specific period of time*"
+   + "\n* or the amount of calorie in and out for a certain period*"
+   +"\n* Your will learn about your BMI as your enter your weight, height and age*"
+   + "\n a panoply of choice will be display based on your BMI*"
+   +"\n*your BMI will be in a range from Severe thin to obesity type III*"
+   + "\n* you will build your meal and choose the approprate workout based on your BMI *"
+   +"\n*******************"
+   +"\n* Good luck and have fun in this adventure.*"
+   +"\n**************"
+   +"\n********************"
+        
+    );
+    }
+   
+     
+     /**
+      *display the start program view
+      */
+    public void displayStartProgramView() {
+        
+      boolean done = false; // set  flag to not done
+      do {
+     // prompt for and get players name
+     String playersName = this.getPlayersName();
+     if(playersName.toUpperCase().equals("Q"))// user wants to quit
+     return; // exit the game
+    
+      // do request action and display next view
+      done = this.doAction(playersName);
+      } while (!done);
+}
+
+    private String getPlayerName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private String getPlayersName() {
+     System.out.println("\ngetPlayersName () called ****");
+     return "Sullen";
+    }
+
+    private boolean doAction(String playersName) {
+        System.out.println("\n***doActoin() called ***");
+        return true;
+            }
+}
