4d51e61b53930c121f1cfd2db9f8b7fc5996dd82
die-zarry <zarrydie03@gmail.com>
2017-06-07 15:56:49 -0500

    Merge origin/master
    Conflicts:
    	README.md
    have the first part done
    still strugle but done with the "doAction" function
    
    Still strugle but done with mainMenuView class
    
    done with the implementation of display mainmenuview function

--- obesity-fight/src/citbyui/cit260/obesity/view/MainMenuView.java
+++ obesity-fight/src/citbyui/cit260/obesity/view/MainMenuView.java
@@ -5,12 +5,15 @@
  */
 package citbyui.cit260.obesity.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author wdc
  */
 public class MainMenuView {
     private String menu;
+    private String promptMessage;
 
     public MainMenuView(){
     this.menu = "\n"
@@ -44,14 +47,70 @@ public class MainMenuView {
     }
 
     private String getMenuOption() {
-        System.out.println("n*** getMenuOption () function called**");
-    return "N";
+        
+        /*
+        *ok get this to the getplayersAnme ()function
+        */
+         Scanner keyboard = new Scanner (System.in); //get infile keyborad 
+       String  value = ""; // value to be return 
+       boolean valid = false; // initiallize to not valaid
+       
+       while (!valid) { // loop while invalid is entered
+       System.out.println("\n" + this.promptMessage);
+       
+       value = keyboard.nextLine();// get next line type on keyboard
+       value = value.trim(); // trim off leading and trailing blanks
+       
+       
+       if (value.length() < 1) { // value is blank
+           System.out.println("\nIvalid value: value can not be one blank");
+       continue;
+      
+       }
+       break; // end the loop
+       }
+       return value; // return the value entered
+    }    
+        
+     
+    private boolean doAction(String choice) {
+        choice = choice.toUpperCase(); //convert choice to upper case
+    switch (choice){
+        case "N": // create and start a new game
+              this.startNewGame();
+              break;
+        case "G": // get and start an existing game
+              this.startExistingGame();
+              break;
+        case "H": //display the help menu
+               this.displayHelpMenu();
+               break;
+        case "S": // save curent game
+               this.saveGame();
+               break;
+        default:
+             System.out.println("\n****Invalid selection**** try again");
+
+    }
+        
+        return false;
+    
     }
 
-    private boolean doAction(String menuOption) {
-        System.out.println("\n*** doAction () function called *****");
-    return true;
-    
+    private void startNewGame() {
+       System.out.println("****startNewGame function ");
+    }
+
+    private void startExistingGame() {
+     System.out.println("****startExistingGame function called ");   
+    }
+
+    private void saveGame() {
+        System.out.println("****startExistingGame function called "); 
+    }
+
+    private void displayHelpMenu() {
+       System.out.println("****displayHelpmenu function called ");  
     }
          
     
