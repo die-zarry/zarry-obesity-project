cc2ac6438de36a801c538ea1c105af5af11cf7dd
die-zarry <zarrydie03@gmail.com>
2017-06-06 19:45:11 -0500

    Merge origin/master
    Conflicts:
    	README.md
    have the first part done

--- obesity-fight/src/citbyui/cit260/obesity/view/StartProgramView.java
+++ obesity-fight/src/citbyui/cit260/obesity/view/StartProgramView.java
@@ -11,67 +11,63 @@ package citbyui.cit260.obesity.view;
  */
 public class StartProgramView {
     private String promptMessage;
+
     public StartProgramView(){
-    this.promptMessage = "\nPlease enter your name: ";
+this.promptMessage = "\nPlease enter your name: ";
     // display the banner when view is created
-    this.displayBanner();
-    
-  }
+     this.displayBanner();
+    }
 
     private void displayBanner() {
-       
-        System.out.println(
-        "\n********************"
+    System.out.println(
+     "\n********************"
    + "\n*                    *"
    + "\n* This is the game to fight Obesity*"
    + "\n* In this game you will fight against obesity by contolling your diet *"
    +"\n* and by selecting the appropriate exercice to do. *"
    + "\n* Your goal is to fight aganst obesity*"
    + "\n* your action will be the food you eat and the appropriate selected excercice*"
-   + "\n* you will measure your performance( winning or lossing)either according to your weight"
-   + "\n *loss or gain durring a specific period of time*"
+   + "\n* you will measure your performance( winning or lossing)either according to your weight*"
+   + "\n*loss or gain durring a specific period of time*"
    + "\n* or the amount of calorie in and out for a certain period*"
    +"\n* Your will learn about your BMI as your enter your weight, height and age*"
-   + "\n a panoply of choice will be display based on your BMI*"
-   +"\n*your BMI will be in a range from Severe thin to obesity type III*"
+   + "\n* a panoply of choice will be display based on your BMI*"
+   +"\n* your BMI will be in a range from Severe thin to obesity type III*"
    + "\n* you will build your meal and choose the approprate workout based on your BMI *"
    +"\n*******************"
    +"\n* Good luck and have fun in this adventure.*"
-   +"\n**************"
-   +"\n********************"
-        
+   +"\n****                                                                       *"
+   +"\n************************************************************************************"
     );
     }
-   
-     
-     /**
-      *display the start program view
-      */
-    public void displayStartProgramView() {
-        
-      boolean done = false; // set  flag to not done
-      do {
-     // prompt for and get players name
-     String playersName = this.getPlayersName();
-     if(playersName.toUpperCase().equals("Q"))// user wants to quit
-     return; // exit the game
-    
-      // do request action and display next view
-      done = this.doAction(playersName);
-      } while (!done);
-}
 
-    private String getPlayerName() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    /***
+     * display the start program view
+     */
+    public void displayStartProgramView() {
+      boolean done = false; // set flag to not done
+      do {
+          // prompt for and get players name
+          String playersName = this.getPlayersName();
+      
+          if (playersName.toUpperCase().equals("Q")) // user wants to quit
+           return; // exit the game
+          
+         // do the requested action and display the next view
+           done = this.doAction(playersName);
+           
+      } while(!done);
+           
     }
 
     private String getPlayersName() {
-     System.out.println("\ngetPlayersName () called ****");
-     return "Sullen";
+   System.out.println("\n*** doAction() called***");
+    return "sullen";
+    }
+    private boolean doAction(String playersName) {
+        System.out.println("\n*** doAction() called***");
+    return true;
     }
 
-    private boolean doAction(String playersName) {
-        System.out.println("\n***doActoin() called ***");
-        return true;
-            }
+    
 }
