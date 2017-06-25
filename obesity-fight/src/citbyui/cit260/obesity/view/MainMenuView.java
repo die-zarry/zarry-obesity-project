380f35acf5f53187dd020712eeeebf68d1b491fe
die-zarry <zarrydie03@gmail.com>
2017-06-24 22:35:26 -0500

    Merge origin/master
    Conflicts:
    	README.md
    have the first part done
    still strugle but done with the "doAction" function
    
    Still strugle but done with mainMenuView class
    
    done with the implementation of display mainmenuview function
    
    
    Finally done with the help function
    Some corection in the helpmenu view function.
    
    I am done with one View assignment but cat figure out how to do the second view one.
    Week 9 update

--- obesity-fight/src/citbyui/cit260/obesity/view/MainMenuView.java
+++ obesity-fight/src/citbyui/cit260/obesity/view/MainMenuView.java
@@ -1,12 +1,16 @@
+package citbyui.cit260.obesity.view;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package citbyui.cit260.obesity.view;
+
 
 import byui.cit260.obesity.control.GameControl;
+import byui.cit260.obesity.control.IndividualBmiControl;
 import byui.cit260.obesity.model.GameMenuView;
+import citbyui.cit260.obesity.view.HelpMenuView;
 import java.util.Scanner;
 import obesity.fight.Obesity;
 
@@ -14,11 +18,9 @@ import obesity.fight.Obesity;
  *
  * @author wdc
  */
-public class MainMenuView {
-    private String menu;
-
-    public MainMenuView(){
-    this.menu = "\n"
+public class MainMenuView extends View {
+           public MainMenuView(){
+                super("\n                            "
                 + "\n------------------------------------"
                 + "\n| Main Menu                         |"
                 +"\nN - Start new Game"
@@ -26,87 +28,23 @@ public class MainMenuView {
                 +"\nH - Get help on how to play the game"
                 +"\nS - Save game"
                 +"\nQ- Quit"
-                +"\n--------------------------------------";
-            
-        
-    }
-    
+                +"\n--------------------------------------");
+            }
+
     /**
      * display the start program view
      *
      */
-    public void displayMainMenuView() {
-        boolean done = false; // set flag to not done
-        do{
-            // prompt for  and get players name
-            String menuOption = this.getMenuOption();
-            if(menuOption.toUpperCase().equals("Q"))// user wants to quit
-            return; // exit the game
-            
-            // do the requested action and display the next view
-            done = this.doAction(menuOption);
-        }while(!done);
-    }
-
-    private String getMenuOption() {
+     @Override 
+    public boolean doAction(String value) {
+        value = value.toUpperCase(); // convert to all upper case
+               return false;
         
-        /*
-        *ok get this to the getplayersAnme ()function
-        */
-         Scanner keyboard = new Scanner (System.in); //get infile keyborad 
-       String  value = ""; // value to be return 
-       boolean valid = false; // initiallize to not valaid
-       
-       while (!valid) { // loop while invalid is entered
-       System.out.println("\n" + this.menu);
-       
-       value = keyboard.nextLine();// get next line type on keyboard
-       value = value.trim(); // trim off leading and trailing blanks
-       
-       
-       if (value.length() < 1) { // value is blank
-           System.out.println("\nIvalid value: value can not be one blank");
-       continue;
-      
-       }
-       break; // end the loop
-       }
-       return value; // return the value entered
-    }    
-        
-     
-    public boolean doAction(String choice) {
-        choice = choice.toUpperCase(); //convert choice to upper case
-    switch (choice){
-        case "N": // create and start a new game
-              this.startNewGame();
-              break;
-        case "G": // create a new game menu view
-              this.createGameMenu();
-              break;
-        case "H": //display the help menu
-               this.displayHelpMenu();
-               break;
-        case "S": // save curent game
-               this.saveGame();
-               break;
-        case "BMI": // display BMI range
-               this.displayBmiMenu();
-               break; 
-               
-        default:
-             System.out.println("\n**** Ivalid selection**** try again");
-
-    }
-        
-        return false;
     
     }
-
     private void startNewGame() {
-        // Create e new game
+        // Create  new game
        GameControl.createNewGame(Obesity.getPlayer());
-    
       // display the game view
        GameMenuView gameMenu = new GameMenuView();
      gameMenu.dysplayMenu();
@@ -120,7 +58,7 @@ public class MainMenuView {
        GameControl.createNewGame(Obesity.getPlayer());
        // dysplay help menu
        HelpMenuView helpMenu = new HelpMenuView();
-       helpMenu.displayHelpMenuView();    }
+       helpMenu.display();    }
  
     private void saveGame() {
         System.out.println("****saveGame function called "); 
@@ -129,12 +67,24 @@ public class MainMenuView {
     private void displayBmiMenu() {
         GameControl.createNewGame(Obesity.getPlayer());
        // dysplay BMI menu
-       BmiMenuView bmiMenu = new BmiMenuView();
-       bmiMenu.displayBmiMenuView();   
+        BmiMenuView BmiMenu = new BmiMenuView();
+       BmiMenu.display(); 
     }
 
-   
-         
-    
-    
+
+    private void displayIndividualBmiControl() {
+     GameControl.createNewGame(Obesity.getInput());
+       // dysplay Individual BMI
+       IndividualBmiControl IndividualBmiControlMenu = new IndividualBmiControl();
+       IndividualBmiControlMenu.displayIndividualBmiControl();  
+    }
+
+    private static class CalcBmiOfIndividualMenuView {
+
+        public CalcBmiOfIndividualMenuView() {
+        }  
+    }
+
 }
+
+    
