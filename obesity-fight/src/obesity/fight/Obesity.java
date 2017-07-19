d930d514a4df7448336c5dc669f370b21f065808
die-zarry <zarrydie03@gmail.com>
2017-07-19 16:38:18 -0400

    Merge origin/master
    Conflicts:
    	README.md
    have the first part done
    still strugle but done with the "doAction" function
    
    Still strugle but done with mainMenuView class
    
    done with the implementation of display mainmenuview function
    
    
    Finally done with the help function
    Some corection in the helpmenu view function.
    
    I am done with one View assignment but cat figure out how to do the second view one.
    Week 9 update
    Week 10 Array and enum
    Week 11

--- obesity-fight/src/obesity/fight/Obesity.java
+++ obesity-fight/src/obesity/fight/Obesity.java
@@ -8,6 +8,12 @@ package obesity.fight;
 import byui.cit260.obesity.model.Game;
 import byui.cit260.obesity.model.Player;
 import citbyui.cit260.obesity.view.StartProgramView;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 
@@ -21,7 +27,30 @@ import citbyui.cit260.obesity.view.StartProgramView;
 public class Obesity {
   private static Game currentGame = null;
   private static Player player = null;
+  
+  private static PrintWriter outFile = null;
+  private static BufferedReader infile = null;
+    
+  private static PrintWriter logFile = null;
+          private static BufferedReader inFile;
 
+    public static PrintWriter getOutFile() {
+        return outFile;
+    }
+
+    public static void setOutFile(PrintWriter outFile) {
+        Obesity.outFile = outFile;
+    }
+
+    public static BufferedReader getInfile() {
+        return infile;
+    }
+
+    public static void setInfile(BufferedReader infile) {
+        Obesity.infile = infile;
+    }
+
+  
     public static Game getCurrentGame() {
         return currentGame;
     }
@@ -40,18 +69,66 @@ public class Obesity {
   
   
 
-    public static void main(String[] args) {
-    
-   // create StartProgramView0rig and display start program view
+    public static void main(String[] args) throws FileNotFoundException {
+        
+        try{
+            // open character stream files for end  user input and output 
+        } Obesity.inFile =
+                new BufferedReader (new InpputStreamReader(System.in));
+           Obesity.outFile = new PrintWriter(System.out, true);
+           
+          Obesity.outFile = new PrintWriter(System.out, true);
+   
+           // open log file 
+           String filePath = "log.txt"
+           Obesity.logFile = new PrintWriter(filePath);
+// create StartProgramView0rig and display start program view
    StartProgramView startProgramView = new StartProgramView();
   startProgramView.displayStartProgramView();
   
-    } 
+    
+    }}
+
+    public static Player getInput() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+      catch(Throwable e){
+    
+    System.out.println("Exception:" + e.toString())+
+                         "\nCause:" + e.getCause() +
+                         "nMessage: " + e.getMessage());
+                         
+                         e.printStackTrace();;
+}
+
+finally {
+      try {
+         if(Obesity.inFile ! = null)
+           Obesity.inFile.close();
+           if (Obesity.outFile ! = null)
+           Obesity.outFile.close();
+      } catch (IOException ex) {
+          System.out.println("Error closing files");
+      }
+}
+}
+    Obesity.outFile.close();
+
+    class outFile {
+
+        public outFile() {
+        }
+
+        private static class close {
+
+            public close() 
+            
+        }
+    }
+    
 
     public static Player getInput() {
         System.out.println("\n***displaygetInput() function called****");
       return null;
       
-    }
-
 }
