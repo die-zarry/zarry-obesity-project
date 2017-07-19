abfb8532784462636fa4ff06d64d834936f0a112
die-zarry <zarrydie03@gmail.com>
2017-07-19 08:30:09 -0400

    Merge origin/master
    Conflicts:
    	README.md
    have the first part done
    still strugle but done with the "doAction" function
    
    Still strugle but done with mainMenuView class
    
    done with the implementation of display mainmenuview function
    
    
    Finally done with the help function
    Some corection in the helpmenu view function.
    
    I am done with one View assignment but cat figure out how to do the second view one.
    Week 9 update
    Week 10 Array and enum
    Week 11

--- obesity-fight/src/byui/cit260/obesity/model/Game.java
+++ obesity-fight/src/byui/cit260/obesity/model/Game.java
@@ -101,6 +101,10 @@ public class Game implements Serializable{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    public Map getMap() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     
     
    
